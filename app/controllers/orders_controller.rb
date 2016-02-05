89aa54bf0669a97f7082ce1d80e028826ce85d61
Cyrus Khajvandi <cskhajvandi@gmail.com>
2016-02-04 22:14:34 -0800

    create actions for controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,38 @@
 class OrdersController < ApplicationController
-    def index
-    end
+
+        def index
+        end
+
+        def show
+        end
+
+        def new
+            @order = Order.new
+        end
+
+        def create
+            @order = order.new(order_params)
+            if @order.save
+                redirect_to @order
+            else
+                render 'new'
+            end
+        end
+
+        def edit
+        end
+
+        def update
+        end
+
+        def destroy
+        end
+
+    private
+
+        def order_params
+        end
+
+        def find_order
+        end
 end
