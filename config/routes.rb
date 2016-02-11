bf82ff1490769f7b21f275490070e9dc3ce5973e
Cyrus Khajvandi <cskhajvandi@gmail.com>
2016-02-10 20:40:58 -0800

    created nested routes for pickup and delivery models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders do  #creating a nested route so making it a block
+    resources :deliveries
+    end 
   root 'orders#index'
 end
